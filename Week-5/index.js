95f297dddf78f9b88a2c5f008628229fc41c01c0
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2022-06-12 21:21:16 +0530

    modified by adding generator() and mentalMaker() functions and completed the assignment

--- Week-5/index.js
+++ Week-5/index.js
@@ -1,11 +1,73 @@
-//Async and Await approach
-async function displayCommits() {
-  const user = await getUser(1);
-  const repos = await getRepositories(user.gitHubUsername);
-  const commits = await getCommits(repos);
-  console.log(commits);
+// //Async and Await approach
+// async function displayCommits() {
+//   const user = await getUser(1);
+//   const repos = await getRepositories(user.gitHubUsername);
+//   const commits = await getCommits(repos);
+//   console.log(commits);
+// }
+// displayCommits();
+
+
+
+// generator function* () approach.
+// generator function* ().
+mentalMaker(function* () {
+  const id = 1;
+  const user = yield getUser(id);//returns UserObject{}
+  const repositories = yield getRepositories(user.gitHubUsername); //returns array of Repositories.
+  const commits = yield getCommits(repositories); //returns array of commits.
+  console.log(commits); //prints commits.
+});
+
+function mentalMaker(generator) {
+  const myGenerator = generator();
+  const execute_generator = myGenerator.next(); // returns Iterator Object --value = Pending Promise.
+  const promise = execute_generator.value;
+  promise.then(value => { 
+    console.log(`User = `, value);
+    const execute_generator2 = myGenerator.next(value);
+    /* returns { id: id, gitHubUsername: 'mosh' } to store in user at line 16 and resumes generator function to continue execution.
+       in this step, here 3 things happens-
+        1. returning the result of the value = { id: id, gitHubUsername: 'Pradyumna-Hegde' }
+        2. resuming the generator() function to continue execution and
+        3. Line 17 executes and returns generator object containing Pending Promise Object.
+    */
+
+    const promise2 = execute_generator2.value;
+    promise2.then(value => {
+      console.log(`Array of repositories = `, value);
+      const execute_generator3 = myGenerator.next(value);
+       /* returns [ 'repo1', 'repo2', 'repo3' ] to store in repositories at line 17 and resumes generator function to continue execution.
+       in this step, here also 3 things happens-
+        1. returning the result of the value --[ 'repo1', 'repo2', 'repo3' ]
+        2. resuming the generator() function to continue execution and
+        3. Line 18 executes and returns generator object containing Pending Promise Object.
+    */
+
+      const promise3 = execute_generator3.value;
+      promise3.then(value => {
+        // console.log(`Array of Commits = `, value);
+        myGenerator.next(value);
+      });
+    });
+  });
 }
-displayCommits();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* creating the function  to get the user{} object from the database. */
@@ -13,7 +75,7 @@ function getUser(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log('Reading a user from a database...');
-      resolve({ id: id, gitHubUsername: 'mosh' });
+      resolve({ id: id, gitHubUsername: 'Pradyumna-Hegde' });
     }, 2000);
   });
 }
@@ -33,7 +95,7 @@ function getCommits(repo) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log('Calling API...');
-      resolve(['commit']);
+      resolve(['commit-1', 'commit-2', 'commit-3']);
     }, 2000);
   });
 }
