5d46ff644ccb3cf97b7c079797f4d42cfd45ec19
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2022-06-19 13:17:13 +0530

    added exercise-5.js and completed the assignment-6

---
+++ Week-6/exercise-5.js
@@ -0,0 +1,20 @@
+function pair_with_given_difference(array, target) {
+   let result = 0; 
+    for(let i=0; i<array.length; i++) {
+        for(let j=0; j<array.length; j++) {
+            if(compare_and_subtract(array[i], array[j]) === target) {
+                result = 1;
+            }
+        }
+    }
+    return result;
+}
+
+function compare_and_subtract(a, b) {
+    if(a > b) return a-b;
+    else return b-a;
+}
+
+
+const array =  [5, 10, 3, 2, 50, 80];
+console.log(pair_with_given_difference(array, 78));
